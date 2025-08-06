5181ab55396886ab7e397ded765858b3fd09d82d
lzl <1010691812@qq.com>
2025-08-06 10:40:36 +0800

    第四次添加的内容

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Mon Jul 31 11:40:06 2023
-__version__ = '1.2.0+cc63400'
+# TIME: Wed Aug  6 10:37:03 2025
+__version__ = '1.2.0+8d5fd9a'
 short_version = '1.2.0'
 version_info = (1, 2, 0)
